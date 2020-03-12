9b9ff08d080fa281927a50255e93b7c4d27ca8d8
saikarthik <saikarthik.b007@gmail.com>
2020-03-12 19:05:55 +0530

    Design

---
+++ saikarthikDesignPatterns/src/test/java/saikarthik_epamtask7/saikarthikDesignPatterns/AppTest.java
@@ -0,0 +1,20 @@
+package saikarthik_epamtask7.saikarthikDesignPatterns;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
